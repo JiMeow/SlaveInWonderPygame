e4170afdefea16a1ad28fdd061c21624422fcb1e
jiratQ <jirat123@icloud.com>
2022-05-17 00:14:16 +0700

    update docstring card.py

--- helper.py
+++ helper.py
@@ -1,15 +1,36 @@
 
 def resetDictdata(data):
+    """
+    It takes a dictionary as input and removes all the keys from it
+
+    :param data: The dictionary that you want to reset
+    """
     for key in list(data.keys()):
         data.pop(key)
 
 
 def setDictdata(dict, data):
+    """
+    It takes a dictionary and a dictionary of data and sets the values of the first dictionary to the
+    values of the second dictionary
+
+    :param dict: The dictionary to be updated
+    :param data: The data to be added to the dictionary
+    """
     for key in list(data.keys()):
         dict[key] = data[key]
 
 
 def getDataFromServer(network, player, gamestart, data):
+    """
+    It sends a request to the server, and then it resets the data dictionary and then it sets the data
+    dictionary to the new data
+
+    :param network: the network object
+    :param player: The player's name
+    :param gamestart: a boolean that tells the client if the game has started
+    :param data: a dictionary that contains all the data that the server sends to the client
+    """
     newdata = network.send({
         "player": player,
         "gamestart": gamestart,
@@ -20,6 +41,13 @@ def getDataFromServer(network, player, gamestart, data):
 
 
 def setDataFromServer(data, allplayer):
+    """
+    It takes a dictionary of dictionaries, and a dictionary of dictionaries, and it sets the values of
+    the second dictionary to the values of the first dictionary
+
+    :param data: a dictionary
+    :param allplayer: a dictionary of dictionaries
+    """
     resetDictdata(allplayer)
     setDictdata(allplayer, data["allplayer"])
 
