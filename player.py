9a04807bbb249b585b38bb9dd1124c6bbb3ea445
jiratQ <jirat123@icloud.com>
2022-05-17 00:20:13 +0700

    update player doc

--- player.py
+++ player.py
@@ -3,6 +3,12 @@ import pygame
 
 class Player:
     def __init__(self, id, color):
+        """
+        This function is used to create a player object
+
+        :param id: The player's id
+        :param color: The color of the player
+        """
         self.id = id
         self.color = color
         self.name = f"player{id}"
@@ -13,6 +19,14 @@ class Player:
         self.iscompleteturn = False
 
     def draw(self, screen, x, y):
+        """
+        It draws a rectangle with the color of the object, and then it draws the name of the object in
+        the center of the rectangle.
+
+        :param screen: the screen you want to draw the button on
+        :param x: x coordinate of the top left corner of the rectangle
+        :param y: the y coordinate of the top left corner of the rectangle
+        """
         pygame.draw.rect(screen, self.color, (x, y, self.width, self.high))
         textname = pygame.font.Font(None, int(20)).render(
             f"{self.name}", True, "white")
