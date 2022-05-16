2f8d0971edeee1165a2f21576c296a92bce37f7d
jiratQ <jirat123@icloud.com>
2022-05-17 00:20:06 +0700

    update network doc

--- network.py
+++ network.py
@@ -10,18 +10,32 @@ port = 5555
 
 class Network:
     def __init__(self):
+        """
+        It creates a socket object.
+        """
         self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.server = server
         self.port = port
         self.addr = (self.server, self.port)
 
     def getInitData(self):
+        """
+        It connects to the database and returns the connection object
+        :return: The return value is the result of the connect() method.
+        """
         return self.connect()
 
     def disconnect(self):
+        """
+        The function disconnects the client from the server
+        """
         self.client.close()
 
     def connect(self):
+        """
+        It connects to the server and returns the data that the server sends
+        :return: The server is returning a list of all the files in the directory.
+        """
         try:
             self.client.connect(self.addr)
             return pickle.loads(self.client.recv(65536))
@@ -29,6 +43,12 @@ class Network:
             print(e)
 
     def send(self, data):
+        """
+        It sends data to the server, and then waits for a response
+
+        :param data: The data to send to the server
+        :return: The return value is a list of tuples.
+        """
         packet = []
         self.client.send(pickle.dumps(data))
         while True:
