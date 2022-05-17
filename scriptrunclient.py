04325fefed0319c85f637c032a383060865f9350
jiratQ <jirat123@icloud.com>
2022-05-17 19:27:00 +0700

    update script run game (for debug)

---
+++ scriptrunclient.py
@@ -0,0 +1,20 @@
+import os
+from threading import Thread
+from time import sleep
+import sys
+
+
+def run_client(script):
+    os.system(script)
+
+
+p_count = int(sys.argv[1])
+script = "client.py"
+all_p = []
+for i in range(p_count):
+    p = Thread(target=run_client, args=(script,))
+    all_p.append(p)
+for i in range(p_count):
+    all_p[i].start()
+    print(f"Started P{i+1}")
+    sleep(0.3)
